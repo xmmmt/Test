999fa089c734429638c64c97140210441aee7224
root <root@localhost.localdomain>
2018-09-27 20:47:48 -0400

    add day03.py

---
+++ day03.py
@@ -0,0 +1,59 @@
+'''
+#EP:1
+for i in 'abcdef':
+    print(i)
+
+#EP:2
+i = 0
+a = 'abcde'
+while i < len(a):
+    print(a[i])
+    i += 1
+
+#EP:3
+num = int(input("请猜测一个数字(0-9)： "))
+a = 4
+while num != a :
+    num = int(input("再猜: "))
+print("congratulation!")
+
+#EP:4
+import random
+num = random.randint(1,5)
+input_num = int(input("input>>"))
+
+#EP:5
+import numpy as np
+#i = 1
+#while != 0:
+#    print(i)
+#    i -= 0.1
+np.zeros(shape = (3,3,3,3) )
+
+#EP:6
+#sum_ = 0
+#for i in range (1001):
+#    sum_ = sum_ + i
+#print(sum_)
+i = 1 
+sum_ = 0
+while sum_< 10000:
+    sum_ += i
+    i += 1
+print(sum_)
+
+#EP:7
+i = 1
+sum_ = 0
+for i in range (142):
+    sum_ = sum_ + i
+print(sum_)
+
+#EP:8
+from _future_ import print print_function
+for i in range(1,10):
+    for i in range(1,j+1):
+        print
+'''
+#EP:9
+
